115c697362fc0302c48bc1c4c99b1549848b1d19
semgoSE <semgoSE@gmail.com>
2020-09-17 22:25:01 +0300

    new v

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 import Persik from './panels/Persik';
+import { PanelHeader, Panel, Div, Placeholder, Root, Button, PanelHeaderBack, Input, FormLayout, Card, SimpleCell, Textarea, Separator, Checkbox } from '@vkontakte/vkui';
+import { Icon56AddCircleOutline, Icon56GalleryOutline } from '@vkontakte/icons';
 
 class App extends Component {
 	state = {
 		activePanel: 'home',
-	
+		form: {
+
+		},
 	}
 
 	go = (activePanel) => {
@@ -23,7 +27,35 @@ class App extends Component {
 			<Root>
 				<View activePanel={activePanel}>
 					<Panel id='home'>
-			
+						<PanelHeader>Подкасты</PanelHeader>
+						<Placeholder action={<Button onClick={() => this.go('create_podcats_1')}>Добавить подкаст</Button>} header="Добавьте первый подкаст" icon={<Icon56AddCircleOutline />} stretched>
+							Добавляйте, редактируйте и делитесь подкастами вашего сообщества.
+						</Placeholder>
+					</Panel>
+
+					<Panel id='create_podcats_1'>
+						<PanelHeader left={<PanelHeaderBack onClick={() => this.go('home')}/>}>Новый подкаст</PanelHeader>
+						<SimpleCell disabled before={<Card mode='tint'><div><Icon56GalleryOutline fill="var(--accent)"/></div></Card>}>
+							<FormLayout><Input placeholder='Введите название подкаста' top="Название"/></FormLayout>
+						</SimpleCell>
+						<FormLayout>
+							<Textarea top="Описание подкаста"/>
+						</FormLayout>
+						<Div>
+							<Placeholder action={<Button mode='outline'>Загрузить файл</Button>} header="Загрузите Ваш подкаст">Выберите готовый аудиофайл из вашего телефона и добавьте его</Placeholder>
+						</Div>
+						<Separator />
+						<FormLayout>
+							<Checkbox>Ненормативный контент</Checkbox>
+							<Checkbox>Исключить эпизод из экспорта</Checkbox>
+							<Checkbox defaultChecked>Трейлер подкаста</Checkbox>
+						</FormLayout>
+						<SimpleCell description="Всем пользователям">Кому доступен данный подкаст</SimpleCell>
+						<SimpleCell multiline description="При публикации записи с эпизодом, он становится доступным для всех пользователей">
+						</SimpleCell>
+						<Div>
+							<Button size='xl'>Дальше</Button>
+						</Div>
 					</Panel>
 				</View>
 			</Root>
