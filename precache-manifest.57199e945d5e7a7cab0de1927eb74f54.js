f16ba7a42a5a70da26299b9f0de496f00b5b625a
semgoSE <semgoSE@gmail.com>
2020-09-18 11:15:52 +0300

    Updates

---
+++ precache-manifest.57199e945d5e7a7cab0de1927eb74f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d971e3b2141e2c66bf53c4c7cca750",
+    "url": "/podcasts/index.html"
+  },
+  {
+    "revision": "8590f4cdbed5ea8e07dc",
+    "url": "/podcasts/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "c8b8ff1330a2885b3c4c",
+    "url": "/podcasts/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8590f4cdbed5ea8e07dc",
+    "url": "/podcasts/static/js/2.f105a708.chunk.js"
+  },
+  {
+    "revision": "c8b8ff1330a2885b3c4c",
+    "url": "/podcasts/static/js/main.3df306d1.chunk.js"
+  },
+  {
+    "revision": "7e0a0a792d5feaf8875a",
+    "url": "/podcasts/static/js/runtime-main.5622a152.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/podcasts/static/media/persik.4e1ec840.png"
+  }
+]);
